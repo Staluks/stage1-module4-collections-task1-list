7bb85611838f09d811a27e1a05306be6c7a669e4
Staluks <janchukssss@inbox.lv>
2023-01-24 16:09:44 +0200

    finished two tasks of 1.4.1

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,26 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        List<Integer> newList = new ArrayList<>();
+        Integer member = 0;
+        for(String i : sourceList){
+            int x = Integer.parseInt(i);
+            member = 5*x^2+3;
+            newList.add(member);
+        }
+        System.out.println(newList);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+    int x = 0;
+        return x;
     }
 }
